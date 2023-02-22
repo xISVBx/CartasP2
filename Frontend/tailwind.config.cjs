628de26661f680f6c76b4c51fe33145143100cdd
ivan vasuqez <ivansantiagovb@gmail.com>
2023-02-22 08:00:43 -0500

    arreglo del diseno de la carta

--- Frontend/tailwind.config.cjs
+++ Frontend/tailwind.config.cjs
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**.{js,ts,jsx,tsx}",
+    "./src/Components/**.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
